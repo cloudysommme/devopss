74126f413f337d838a9a51f6c19f239a93087968
admin <nazik@nazik.myguest.virtualbox.org>
2025-01-16 01:02:41 +0200

    Updated FuncA to return sum of first 3 elements

--- func.cpp
+++ func.cpp
@@ -1,6 +1,11 @@
 #include "func.h"
 
 double HyperbolicTangent::FuncA(double x) {
-    return 0;
+    double result = 0;
+    for (int i = 1; i <= 3; ++i) {
+        double term = (std::pow(4, i) * (std::pow(4, i) - 1) * std::pow(x, 2 * i - 1)) / std::tgamma(2 * i + 1);
+        result += term;
+    }
+    return result;
 }
 
