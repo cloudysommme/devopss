3c24446004797c63447c86314b172a3a66a34aeb
admin <nazik@nazik.myguest.virtualbox.org>
2025-01-16 01:05:46 +0200

    Finished FuncA implementation with comments about parameters

--- func.cpp
+++ func.cpp
@@ -1,6 +1,12 @@
 #include "func.h"
+#include <cmath>
 
-double HyperbolicTangent::FuncA(double x) {
-    return 0;
+double HyperbolicTangent::FuncA(double x, int n) {
+    double result = 0;
+    for (int i = 1; i <= n; ++i) {
+        double term = (std::pow(4, i) * (std::pow(4, i) - 1) * std::pow(x, 2 * i - 1)) / std::tgamma(2 * i + 1);
+        result += term;
+    }
+    return result;
 }
 
